2a0e8b41b039d2dccf255b90d0b02195f6306d55
Daniel <daniel.fiola@outlook.com>
2021-03-29 17:47:31 -0400

    Sans firebase

--- src/composants/Accueil.jsx
+++ src/composants/Accueil.jsx
@@ -1,22 +1,8 @@
 import './Accueil.scss';
-import firebase from 'firebase/app';
-import { instanceFirebaseUI } from '../firebase';
-import 'firebaseui/dist/firebaseui.css';
-import { useEffect } from 'react';
+
 
 
 export default function Accueil() {
-  /*useEffect(
-    () => instanceFirebaseUI.start("#firebaseui-widget", {
-      signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        firebase.auth.GithubAuthProvider.PROVIDER_ID
-      ],
-      signInFlow: 'popup'
-    }, [])
-  )*/
-
   return (
     <div className="contenu acceuil_contenu">
              <h1>Je m'appelle Daniel et voici mon portfolio!</h1>
